8e838c494234b0c64a98033e3d41db59789af7a6
Nahuel Gaston Lopez <127252397+NaweGl@users.noreply.github.com>
2023-04-10 23:44:45 -0300

    Add files via upload

---
+++ src/helpers/index.js
@@ -0,0 +1,46 @@
+const formatearDinero = (valor)=> {
+
+    const formatter = new Intl.NumberFormat('en-US', {
+
+        style: 'currency',
+        currency: 'USD'
+    })
+
+    return formatter.format(valor)
+}
+
+const calcularTotalPagar = (cantidad, plazo) =>{
+
+let total
+
+// Minetras mayor es la cantidad, menor es el interes
+if (cantidad < 5000) {
+    total = cantidad * 1.5;
+}else if (cantidad >= 5000 && cantidad < 10000){
+    total = cantidad * 1.4;
+}else if (cantidad >= 1000 && cantidad < 15000){
+    total = cantidad * 1.3;
+}else 
+    total = cantidad * 1.2;
+
+    // Mas plazo, mayor interes
+
+if(plazo === 6){
+total *= 1.1;
+
+}else if(plazo=== 12){
+total *= 1.2;
+
+}else{
+    total *= 1.3;
+}
+
+return total;
+
+}
+
+export {
+
+    formatearDinero,
+    calcularTotalPagar
+}
